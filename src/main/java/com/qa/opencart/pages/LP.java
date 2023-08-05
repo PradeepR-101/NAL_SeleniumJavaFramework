e320bb3b2f73a8e6d902253a2717694603a8dd38
PradeepR-101 <78174048+PradeepR-101@users.noreply.github.com>
2023-08-05 07:35:34 +0530

    Create LP.java

---
+++ src/main/java/com/qa/opencart/pages/LP.java
@@ -0,0 +1,18 @@
+package com.qa.opencart.pages;
+
+public class LP {
+	
+	int k=0;
+  int a=0; int b=0;
+  String name = "conflicts"
+
+  public void sample(){
+    
+  }
+
+  public void sampleMath(){
+    k=a+b;
+  }
+
+
+}
