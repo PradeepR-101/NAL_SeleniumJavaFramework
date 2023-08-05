020861329264dbc16973e2cf56b779580f2d2cd8
PradeepRaj@1 <rpradee666@gmail.com>
2023-08-05 07:43:22 +0530

    deleted LP111 and modified LP

--- src/main/java/com/qa/opencart/pages/LP.java
+++ src/main/java/com/qa/opencart/pages/LP.java
@@ -1,7 +1,20 @@
 package com.qa.opencart.pages;
 
 public class LP {
+
+	int k = 0;
+	int a = 1;
+	int b = 2;
+	int p=1;
+	int q=2;
+	int r=0;
+
+	void sampleMath() {
+		k = a + b;
+	}
 	
-	int k=0;
+	void ws() {
+		r = p + q;
+	}
 
 }
